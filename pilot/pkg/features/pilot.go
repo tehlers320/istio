ee701cf71d884230afead267e925680641d1868e
Raghav Grover <ragrover@expediagroup.com>
2023-06-29 12:02:56 -0400

    Use .with while initialising

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -663,6 +663,9 @@ var (
 		"Metric scope rotation interval, set to 0 to disable the metric scope rotation").Get()
 	MetricGracefulDeletionInterval = env.Register("METRIC_GRACEFUL_DELETION_INTERVAL", 5*time.Minute,
 		"Metric expiry graceful deletion interval. No-op if METRIC_ROTATION_INTERVAL is disabled.").Get()
+
+	EnableControllerQueueMetrics = env.RegisterBoolVar("ISTIO_ENABLE_CONTROLLER_QUEUE_METRICS", false,
+		"If enabled, publishes metrics for queue depth, latency and processing times.")
 )
 
 // UnsafeFeaturesEnabled returns true if any unsafe features are enabled.
